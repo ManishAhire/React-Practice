a89c4aa9b969fe84718bf4e3f07b7999e868d424
Manish Ahire <ahiremanishb@gmail.com>
2026-06-11 16:45:17 +0530

    Storing custom components into seperate files

---
+++ React/react-essentials/src/components/CoreConcepts.jsx
@@ -0,0 +1,22 @@
+// function CoreConcepts(props) {
+//   return (
+//     <li>
+//       <img src={props.image} alt={props.title} />
+//       <h3>{props.title}</h3>
+//       <p>{props.description}</p>
+//     </li>
+//   );
+// }
+
+// ALTERNATIVE TO ABOVE FUNCTION - Destructuring the props
+function CoreConcepts({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
+export default CoreConcepts;
