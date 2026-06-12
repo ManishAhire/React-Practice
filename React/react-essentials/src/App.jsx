1362a6a9dc36ef5d17f1fc8d849ebadd2cfe0e03
Manish Ahire <ahiremanishb@gmail.com>
2026-06-12 16:25:22 +0530

    Passed onClick Event as Function to Prop

--- React/react-essentials/src/App.jsx
+++ React/react-essentials/src/App.jsx
@@ -4,6 +4,9 @@ import CoreConcepts from './components/CoreConcepts.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
+  function handleSelect() {
+    console.log('Button Pressed');
+  }
   return (
     <div>
       <Header />
@@ -25,10 +28,10 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Componetns</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={handleSelect}>Componetns</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
           </menu>
         </section>
       </main>
