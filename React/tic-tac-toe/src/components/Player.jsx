1304e4e34d9d0b1a5a9235f5d7eb15b09503ef7a
Manish Ahire <ahiremanishb@gmail.com>
2026-06-17 14:04:43 +0530

    Two way binding - input component - useState

--- React/tic-tac-toe/src/components/Player.jsx
+++ React/tic-tac-toe/src/components/Player.jsx
@@ -2,21 +2,33 @@ import { useState } from 'react';
 
 function Player({ name, symbol }) {
   const [isEditing, setIsEdition] = useState(false);
+  const [playerName, setPlayerName] = useState(name);
 
   function handleEditClick() {
     setIsEdition((prev) => !prev);
   }
 
-  let playerName = <span className="player-name">{name}</span>;
+  function handleChangeName(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editPlayerName = <span className="player-name">{playerName}</span>;
 
   if (isEditing) {
-    playerName = <input type="text" required value={name} />;
+    editPlayerName = (
+      <input
+        type="text"
+        required
+        value={playerName}
+        onChange={handleChangeName}
+      />
+    );
   }
 
   return (
     <li>
       <span className="player">
-        {playerName}
+        {editPlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
